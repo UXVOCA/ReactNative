22bdd25e2c7da26c032c6d459b386c8b32f70075
jinsupark4255 <jin04007@naver.com>
2023-11-22 14:41:46 +0900

    초기환경 테스트

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> 안녕</Text>
+      <Text> 안녕하세요</Text>
       <StatusBar style="auto" />
     </View>
   );
