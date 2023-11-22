10a5a4ad0e3bf5cdbb459893800ff08d1d78bdf1
jinsupark4255 <jin04007@naver.com>
2023-11-23 02:05:42 +0900

    Feat: 테스트 페이지 UI 개발 완료 및 페이지 이동 기능 구현

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import TestPage from "./pages/testpage";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TestPage from "./pages/testpage.js";
+import TodayTestPage from "./pages/todaytestpage.js";
+import ReviewTestPage from "./pages/reviewtestpage.js";
+import WrongTestPage from "./pages/wrongtestpage.js";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <TestPage />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="TestPage" component={TestPage} />
+        <Stack.Screen name="TodayTestPage" component={TodayTestPage} />
+        <Stack.Screen name="ReviewTestPage" component={ReviewTestPage} />
+        <Stack.Screen name="WrongTestPage" component={WrongTestPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
--- pages/testpage.js
+++ pages/testpage.js
@@ -1,12 +1,29 @@
-// HomePage.js
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const HomePage = () => {
+const TestPage = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>홈페이지에 오신 것을 환영합니다!</Text>
-      {/* 추가적인 컨텐츠와 컴포넌트들을 여기에 배치할 수 있습니다. */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("TodayTestPage")}
+      >
+        <Text style={styles.buttonText}>오늘의 단어 테스트</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("ReviewTestPage")}
+      >
+        <Text style={styles.buttonText}>복습 단어 테스트</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("WrongTestPage")}
+      >
+        <Text style={styles.buttonText}>틀린 단어 테스트</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -16,12 +33,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "red",
+    padding: 16, // 각 버튼 사이의 간격을 위해 적절한 패딩을 설정합니다.
+    backgroundColor: "white", // 배경색을 흰색으로 설정합니다.
   },
-  text: {
-    fontSize: 20,
-    color: "white",
+  button: {
+    marginBottom: 40, // 버튼 사이의 간격
+    backgroundColor: "#AABCFD", // 버튼의 배경색
+    borderRadius: 10, // 버튼의 모서리 둥글기
+    paddingVertical: 30, // 상하 패딩
+    paddingHorizontal: 74, // 좌우 패딩
+    width: "80%", // 버튼의 너비
+    elevation: 3, // 안드로이드에서 그림자 효과
+    shadowOpacity: 0.3, // iOS에서 그림자 효과
+    shadowRadius: 4, // iOS에서 그림자 둥근 효과
+    shadowColor: "#000", // iOS에서 그림자 색상
+    shadowOffset: { height: 2, width: 0 }, // iOS에서 그림자 방향
+    borderColor: "#A4A4A4",
+    borderWidth: 1,
+  },
+  buttonText: {
+    color: "white", // 텍스트 색상
+    textAlign: "center", // 텍스트 정렬
+    fontSize: 30, // 텍스트 크기
+    fontWeight: "bold", // 텍스트 굵기
   },
 });
 
-export default HomePage;
+export default TestPage;
